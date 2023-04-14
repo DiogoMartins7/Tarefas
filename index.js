d14e8ca64b59bf8de1eb1fc8704fe0ca0a416ca6
DiogoMartins7 <130646167+DiogoMartins7@users.noreply.github.com>
2023-04-15 00:29:27 +0100

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-// Import stylesheets
-import './style.css';
+const taskInput = document.getElementById('task-input');
+const addTaskButton = document.getElementById('add-task');
+const taskList = document.getElementById('task-list');
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let tasks = [];
+
+function renderTasks() {
+  taskList.innerHTML = '';
+  tasks.forEach((task, index) => {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    const editButton = document.createElement('button');
+    const deleteButton = document.createElement('button');
+
+    span.textContent = task;
+    editButton.textContent = 'Editar';
+    editButton.classList.add('edit');
+    deleteButton.textContent = 'Apagar';
+
+    deleteButton.addEventListener('click', () => {
+      tasks.splice(index, 1);
+      renderTasks();
+    });
+
+    editButton.addEventListener('click', () => {
+      const newTask = prompt('Editar tarefa', task);
+      if (newTask !== null && newTask.trim() !== '') {
+        tasks[index] = newTask.trim();
+        renderTasks();
+      }
+    });
+
+    li.appendChild(span);
+    li.appendChild(editButton);
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+  });
+}
+
+addTaskButton.addEventListener('click', () => {
+  const newTask = taskInput.value.trim();
+  if (newTask !== '') {
+    tasks.push(newTask);
+    taskInput.value = '';
+    renderTasks();
+  }
+});
+
+renderTasks();
